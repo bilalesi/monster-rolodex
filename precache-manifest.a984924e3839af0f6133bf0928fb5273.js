f420932e9cee448033feea178c2d13fae75eb3c6
bilalesi <b_meddah@esi.dz>
2019-10-29 21:03:22 -0700

    Updates

---
+++ precache-manifest.a984924e3839af0f6133bf0928fb5273.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a8037fad3391c5f6e1cc41a7c085b17",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "574ddda11cd186799f56",
+    "url": "/monster-rolodex/static/css/main.39e26fe7.chunk.css"
+  },
+  {
+    "revision": "1fc0b286ce180bad53bf",
+    "url": "/monster-rolodex/static/js/2.efc0b5dd.chunk.js"
+  },
+  {
+    "revision": "574ddda11cd186799f56",
+    "url": "/monster-rolodex/static/js/main.4a43e4bb.chunk.js"
+  },
+  {
+    "revision": "25f1733584131d99b4c9",
+    "url": "/monster-rolodex/static/js/runtime-main.aa750ea8.js"
+  }
+]);
